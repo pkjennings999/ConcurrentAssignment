ce6531e7c7331d9f985f5ca3d684a0868b4d22f8
Patrick Jennings <pkjennings999@gmail.com>
2019-03-28 05:03:51 +0000

    conor test

--- convTester.c
+++ convTester.c
@@ -4,19 +4,19 @@
 int main(void)
 {
     char* tests[6][5] = {
-    {"32", "32", "3", "512", "256"}, //Small 
+    {"16", "16", "1", "1024", "2048"}, //Small 
     {"64", "64", "1", "256", "64"}, //Med
     {"128", "128", "7", "128", "128"}  //Large
     };
 
     //Uncomment to print to a file
     // freopen("FINAL.txt", "w+", stdout);
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 1; i++)
     {
         printf("%s, %s, %s %s, %s\n",  tests[i][0], tests[i][1], tests[i][2], tests[i][3], tests[i][4]);
         char* argArray[6] = {"conv-harness",
         tests[i][0], tests[i][1], tests[i][2], tests[i][3], tests[i][4]};
-        for (int j = 0; j < 2; j++)
+        for (int j = 0; j < 10; j++)
         {
             mainCall(6, argArray);
         }
