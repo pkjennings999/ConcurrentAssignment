28c75cb6ea7c0ca17580312e6337c6e63758734b
Patrick Jennings <pkjennings999@gmail.com>
2019-03-28 01:18:54 +0000

    Smol tests

--- convTester.c
+++ convTester.c
@@ -4,7 +4,7 @@
 int main(void)
 {
     char* tests[6][5] = {
-    {"16", "16", "5", "1024", "128"},
+    {"16", "16", "1", "32", "32"},
     {"32", "32", "3", "512", "256"},
     {"64", "64", "1", "256", "64"},
     {"128", "128", "7", "128", "128"},
@@ -16,8 +16,8 @@ int main(void)
     * Download perf for performance info. perf stat -e
     */
 
-    freopen("speedupTests.txt", "w+", stdout);
-    for (int i = 0; i < 3; i++)
+    freopen("smol.txt", "w+", stdout);
+    for (int i = 0; i < 1; i++)
     {
         printf("%s, %s, %s %s, %s\n",  tests[i][0], tests[i][1], tests[i][2], tests[i][3], tests[i][4]);
         char* argArray[6] = {"conv-harness",
