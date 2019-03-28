bf690f30c7051627402fd25a902d84437d94e71c
Patrick Jennings <pkjennings999@gmail.com>
2019-03-28 04:59:27 +0000

    One last check

--- convTester.c
+++ convTester.c
@@ -5,21 +5,18 @@ int main(void)
 {
     char* tests[6][5] = {
     {"32", "32", "3", "512", "256"}, //Small 
-    {"64", "64", "1", "32", "128"}, //Med
-    {"16", "16", "7", "256", "32"}  //Large
+    {"64", "64", "1", "256", "64"}, //Med
+    {"128", "128", "7", "128", "128"}  //Large
     };
-    /*
-    * Use -S to generate assembly code
-    * Download perf for performance info. perf stat -e
-    */
 
-    freopen("FINAL.txt", "w+", stdout);
-    for (int i = 0; i < 1; i++)
+    //Uncomment to print to a file
+    // freopen("FINAL.txt", "w+", stdout);
+    for (int i = 0; i < 3; i++)
     {
         printf("%s, %s, %s %s, %s\n",  tests[i][0], tests[i][1], tests[i][2], tests[i][3], tests[i][4]);
         char* argArray[6] = {"conv-harness",
         tests[i][0], tests[i][1], tests[i][2], tests[i][3], tests[i][4]};
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < 2; j++)
         {
             mainCall(6, argArray);
         }
