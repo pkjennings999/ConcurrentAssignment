f1cc7715031be67715865db0436af7f461b57ba1
Patrick Jennings <pkjennings999@gmail.com>
2019-03-28 03:46:54 +0000

    hopefully the end

--- convTester.c
+++ convTester.c
@@ -5,10 +5,10 @@ int main(void)
 {
     char* tests[6][5] = {
     {"16", "16", "1", "32", "32"}, //Small everything
-    {"512", "512", "1", "32", "128"}, //Small ord, chan
+    {"256", "256", "1", "32", "128"}, //Small ord, chan
     {"16", "16", "7", "256", "32"}, //Small kern, wid, hei
-    {"16", "16", "1", "64", "64"},
-    {"256", "256", "1", "128", "64"},
+    {"16", "16", "1", "32", "64"},
+    {"32", "32", "1", "64", "32"},
     {"16", "16", "7", "32", "32"}
     };
     /*
@@ -17,7 +17,7 @@ int main(void)
     */
 
     freopen("FINALboth.txt", "w+", stdout);
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 6; i++)
     {
         printf("%s, %s, %s %s, %s\n",  tests[i][0], tests[i][1], tests[i][2], tests[i][3], tests[i][4]);
         char* argArray[6] = {"conv-harness",
